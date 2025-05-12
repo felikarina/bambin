6ed1d464c71ebb192463f3154e3b060d6c4a0d43
Karina <146747979+felikarina@users.noreply.github.com>
2025-05-13 00:20:45 +0200

    add bulma

--- src/components/NavigationSidebar.vue
+++ src/components/NavigationSidebar.vue
@@ -1,17 +1,28 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="greetings">
-    <h1>Cheese</h1>
+  <div class="container mt-5">
+    <div class="columns">
+      <div class="column is-one-quarter">
+        <aside class="menu">
+          <p class="menu-label">Navigation</p>
+          <ul class="menu-list">
+            <li><router-link to="/galerie-photo">Galerie photo</router-link></li>
+            <li><router-link to="/galerie-photo">Journal d'activité</router-link></li>
+            <li><router-link to="/galerie-photo">Messagerie</router-link></li>
+            <li><router-link to="/galerie-photo">Paramètres</router-link></li>
+            <li><router-link to="/galerie-photo">Déconnexion</router-link></li>
+          </ul>
+        </aside>
+      </div>
+      <div class="column">
+        <div class="greetings">
+          <h1>Cheese</h1>
+        </div>
+        <router-link to="/">Hello world</router-link>
+      </div>
+    </div>
   </div>
-  <router-link to="/">Hello world</router-link>
 </template>
 
-<style scoped>
-h1 {
-  font-weight: 500;
-  font-size: 2.6rem;
-  position: relative;
-  top: -10px;
-}
-</style>
+<style scoped></style>
