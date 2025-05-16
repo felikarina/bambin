9399f9001a0a1e8fc893b9b0877bdff4aafb654d
Karina <146747979+felikarina@users.noreply.github.com>
2025-05-16 18:27:31 +0200

    update gallery

--- src/views/PhotoGallery.vue
+++ src/views/PhotoGallery.vue
@@ -4,9 +4,13 @@ import gallery from './../components/gallery.vue'
 </script>
 
 <template>
-  <main class="grid">
-    <NavigationSidebar />
-    <gallery class="mt-6" />
+  <main class="columns">
+    <div class="pr-2 column is-one-third">
+      <NavigationSidebar class="is-hidden-mobile" />
+    </div>
+    <div class="column">
+      <gallery class="mt-6 mx-4" />
+    </div>
   </main>
 </template>
 
