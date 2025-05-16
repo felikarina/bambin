ec066c281431ad7ba0f0c9aca8602242881f3d41
Karina <146747979+felikarina@users.noreply.github.com>
2025-05-16 02:01:15 +0200

    test

--- src/components/NavigationSidebar.vue
+++ src/components/NavigationSidebar.vue
@@ -1,57 +1,98 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="box m-2">
-    <ul>
-      <li>
-        <router-link
-          to="/galerie-photo"
-          class="button is-fullwidth is-outlined my-2 has-background-white has-background-white"
-        >
-          Galerie photoo
-        </router-link>
-      </li>
-      <li>
-        <router-link
-          to="/galerie-photo"
-          class="button is-fullwidth is-outlined my-2 has-background-white"
-          >Journal d'activité</router-link
-        >
-      </li>
-      <li>
-        <router-link
-          to="/galerie-photo"
-          class="button is-fullwidth is-outlined my-2 has-background-white"
-          >Messagerie</router-link
-        >
-      </li>
-      <li>
-        <router-link
-          to="/galerie-photo"
-          class="button is-fullwidth is-outlined my-2 has-background-white"
-          >Paramètres</router-link
-        >
-      </li>
-      <li>
-        <router-link
-          to="/galerie-photo"
-          class="button is-fullwidth is-outlined my-2 has-background-white"
-          >Déconnexion</router-link
-        >
-      </li>
-    </ul>
+  <div
+    class="box m-2 full-height is-flex is-flex-direction-column is-justify-content-space-between"
+  >
+    <div class="">
+      <ul>
+        <li>
+          <router-link
+            to="/galerie-photo"
+            class="button is-fullwidth is-outlined is-link mb-4 p-3 has-background-white is-justify-content-space-between"
+          >
+            <span class="icon">
+              <i class="fas fa-image"></i>
+            </span>
+            Galerie photo
+            <span class="icon">
+              <i class="fas fa-arrow-right"></i>
+            </span>
+          </router-link>
+        </li>
+        <li>
+          <router-link
+            to="/galerie-photo"
+            class="button is-fullwidth is-outlined is-link my-4 p-3 has-background-white is-justify-content-space-between"
+          >
+            <span class="icon">
+              <i class="fas fa-pen-to-square"></i>
+            </span>
+            Journal d'activité
+            <span class="icon">
+              <i class="fas fa-arrow-right"></i>
+            </span>
+          </router-link>
+        </li>
+        <li>
+          <router-link
+            to="/galerie-photo"
+            class="button is-fullwidth is-outlined is-link my-4 p-3 has-background-white is-justify-content-space-between"
+          >
+            <span class="icon">
+              <i class="fas fa-envelope"></i>
+            </span>
+            Messagerie
+            <span class="icon">
+              <i class="fas fa-arrow-right"></i>
+            </span>
+          </router-link>
+        </li>
+      </ul>
+    </div>
+    <div class="">
+      <ul>
+        <li>
+          <router-link
+            to="/galerie-photo"
+            class="button is-fullwidth is-outlined is-link my-4 p-3 has-background-white is-justify-content-space-between"
+          >
+            <span class="icon">
+              <i class="fas fa-gear"></i>
+            </span>
+            Paramètres
+            <span class="icon">
+              <i class="fas fa-arrow-right"></i>
+            </span>
+          </router-link>
+        </li>
+        <li class="">
+          <router-link
+            to="/galerie-photo"
+            class="button is-fullwidth is-outlined is-link mt-4 p-3 has-background-white is-justify-content-space-between"
+          >
+            <span class="icon">
+              <i class="fas fa-power-off"></i>
+            </span>
+            Déconnexion
+            <span class="icon">
+              <i class="fas fa-arrow-right"></i>
+            </span>
+          </router-link>
+        </li>
+      </ul>
+    </div>
   </div>
-  <router-link to="/">Hello world</router-link>
 </template>
 
 <style scoped>
 .box {
   background-color: #b2c3df;
   color: #5a95f4;
-  width: 350px;
-  height: 1024px;
+  max-width: 350px;
+  height: 98vh;
 }
-.button {
+.icon {
   color: #5a95f4;
 }
 </style>
