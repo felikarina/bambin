e23e99656a963592e1109c76f4b0b900f7b1e706
Karina <146747979+felikarina@users.noreply.github.com>
2025-05-16 02:24:07 +0200

    Revert "test"
    This reverts commit 956630f519ed26afab0d357ca45cb7d0b5b31391.

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import 'bulma/css/bulma.css'
-import '@fortawesome/fontawesome-free/css/all.min.css'
 
 const app = createApp(App)
 app.use(router)
