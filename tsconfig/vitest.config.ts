6ae35f6d67c177f47b8487ec1b3a2e95c50bc035
Karina <146747979+felikarina@users.noreply.github.com>
2025-06-13 14:38:12 +0200

    add test coverage

--- tsconfig/vitest.config.ts
+++ tsconfig/vitest.config.ts
@@ -1,15 +1,28 @@
-import { fileURLToPath } from 'node:url'
-import { mergeConfig, defineConfig, configDefaults } from 'vitest/config'
-import viteConfig from '../vite.config'
+import { fileURLToPath } from "node:url";
+import { mergeConfig, defineConfig, configDefaults } from "vitest/config";
+import viteConfig from "../vite.config";
 
 export default mergeConfig(
   viteConfig,
   defineConfig({
     test: {
-      environment: 'jsdom',
-      exclude: [...configDefaults.exclude, 'e2e/**'],
-      include: ['../src/**/*.spec.ts', '../src/**/*.test.ts'],
-      root: fileURLToPath(new URL('./', import.meta.url)),
+      environment: "jsdom",
+      exclude: [...configDefaults.exclude, "e2e/**"],
+      include: ["src/**/*.spec.ts", "src/**/*.test.ts"],
+      root: fileURLToPath(new URL("../", import.meta.url)),
+      coverage: {
+        all: true,
+        reporter: ["text", "html"],
+        reportsDirectory: "coverage",
+        include: ["src/**/*"],
+        exclude: [
+          "src/components/icons/**",
+          "**/*.spec.ts",
+          "**/*.test.ts",
+          "src/main.ts",
+          "src/env.d.ts",
+        ],
+      },
     },
-  }),
-)
+  })
+);
