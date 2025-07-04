57a9a9d7fc7386589f0a4466c0bee9d58a10eaba
Karina <146747979+felikarina@users.noreply.github.com>
2025-07-04 12:15:59 +0200

    tree structure redesign

--- vite.config.ts
+++ vite.config.ts
@@ -5,13 +5,14 @@ import vue from "@vitejs/plugin-vue";
 import vueDevTools from "vite-plugin-vue-devtools";
 
 export default defineConfig({
+  root: "frontend",
   plugins: [vue(), vueDevTools()],
   server: {
     port: 3100,
   },
   resolve: {
     alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+      "@": fileURLToPath(new URL("./frontend/src", import.meta.url)),
     },
   },
 });
